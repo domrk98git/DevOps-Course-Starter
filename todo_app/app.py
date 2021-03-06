c2f7d18e48b804097fc2289d4c3a878bdb2ff650
RaviDommata <rkdommata@RaviDommatas-Mac-mini.local>
2021-03-06 00:10:51 +0000

    add ViewModel

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template,url_for,redirect,request
 from todo_app.data import trelloitems
 from todo_app.flask_config import Config
 import os
+from todo_app.data.view_model import ViewModel
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -24,8 +26,10 @@ def index():
     items += trelloitems.fetch_items(os.getenv('PENDINGID'),'PENDING ITEMS')
     items += trelloitems.fetch_items(os.getenv('DONEID'),'DONE ITEMS')
 
-    return render_template('index.html',items=items)
-    
+    ##return render_template('index.html',items=items)
+    item_view_model = ViewModel(items) 
+    return render_template('index.html', view_model=item_view_model)
+ 
 
 if __name__ == '__main__':
     app.run()
---
+++ todo_app/data/view_model.py
@@ -0,0 +1,18 @@
+class ViewModel:
+    def __init__(self, items):
+        self._items = items
+    @property
+    def items(self): return self._items
+
+    @property
+    def to_do_items(self):
+        todo = [item for item in self._items if item.status == "To Do"]
+        return todo
+        
+    @property
+    def doing_items(self):
+        return [item for item in self._items if item.status == "Doing"]
+
+    @property
+    def done_items(self):
+        return [item for item in self._items if item.status == "Done"]
