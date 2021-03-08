f0a5db1f6a6154fb73c1e7a903c5e71bf2f4ee2b
RaviDommata <rkdommata@RaviDommatas-Mac-mini.local>
2021-03-08 18:51:39 +0000

    Add inegration tests until step3

--- todo_app/app.py
+++ todo_app/app.py
@@ -4,32 +4,34 @@ from todo_app.flask_config import Config
 import os
 from todo_app.data.view_model import ViewModel
 
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-app = Flask(__name__)
-app.config.from_object(Config)
+    @app.route('/Add', methods=['POST'])
+    def add_new():
+        title = request.form["title"]   
+        trelloitems.create_item(title)
+        return redirect(url_for('index'))
 
-@app.route('/Add', methods=['POST'])
-def add_new():
-    title = request.form["title"]   
-    trelloitems.create_item(title)
-    return redirect(url_for('index'))
+    @app.route('/UpdateToDone',methods=['GET'])
+    def update():
+        id = request.args.get('id')
+        trelloitems.UpdateToDone(id)
+        return redirect(url_for('index'))
 
-@app.route('/UpdateToDone',methods=['GET'])
-def update():
-    id = request.args.get('id')
-    trelloitems.UpdateToDone(id)
-    return redirect(url_for('index'))
+    @app.route('/')
+    def index():
+        items = trelloitems.fetch_items(os.getenv('TODOID'),'TODO ITEMS')
+        items += trelloitems.fetch_items(os.getenv('PENDINGID'),'PENDING ITEMS')
+        items += trelloitems.fetch_items(os.getenv('DONEID'),'DONE ITEMS')
 
-@app.route('/')
-def index():
-    items = trelloitems.fetch_items(os.getenv('TODOID'),'TODO ITEMS')
-    items += trelloitems.fetch_items(os.getenv('PENDINGID'),'PENDING ITEMS')
-    items += trelloitems.fetch_items(os.getenv('DONEID'),'DONE ITEMS')
+        ##return render_template('index.html',items=items)
+        item_view_model = ViewModel(items) 
+        return render_template('index.html', view_model=item_view_model)
+    
 
-    ##return render_template('index.html',items=items)
-    item_view_model = ViewModel(items) 
-    return render_template('index.html', view_model=item_view_model)
- 
+    if __name__ == '__main__':
+        app.run()
 
-if __name__ == '__main__':
-    app.run()
+    return app
