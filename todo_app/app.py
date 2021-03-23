dcf696c3f84ec08b1f047b9037f4954351a650f4
RaviDommata <rkdommata@RaviDommatas-Mac-mini.local>
2021-03-23 10:21:37 +0000

    commit changes to apitest

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,12 +1,12 @@
+import os
 from flask import Flask,render_template,url_for,redirect,request
 from todo_app.data import trelloitems
-from todo_app.flask_config import Config
-import os
+#from todo_app.flask_config import Config
 from todo_app.data.view_model import ViewModel
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    #app.config.from_object(Config)
 
     @app.route('/Add', methods=['POST'])
     def add_new():
--- todo_app/data/test_client.py
+++ todo_app/data/test_client.py
@@ -1,8 +1,10 @@
+import os
 import pytest
 import todo_app.app as app
-import dotenv
+from dotenv import find_dotenv, load_dotenv
 import json
 from todo_app.data.item import TrelloItem
+from unittest.mock import patch, Mock
 
 @pytest.fixture
 def client():
@@ -20,3 +22,48 @@ def client():
 def test_index_page(mock_get_requests, client): 
     response = client.get('/')
 
+@patch('requests.request')
+def test_index_page(mock_get_requests, client):
+# Replace call to requests.get(url) with our own function
+    mock_get_requests.side_effect = mock_get_lists
+    response = client.get('/')
+
+
+def mock_get_lists(method,url, params):
+    if url == f"https://api.trello.com/1/lists/{os.getenv('TODOID')}/cards":
+        response = Mock()
+        
+# sample_trello_lists_response should point to some test response data
+        response.json.return_value = str("""[{
+                "id": "12345",
+                "name": "Test to do item",
+                "idList": "67890",
+                "dateLastActivity": "2020-08-01T12:52:06.278Z"
+            }]""")
+
+        return response
+    elif url == f"https://api.trello.com/1/lists/{os.getenv('DONEID')}/cards": 
+        response = Mock()
+        
+# sample_trello_lists_response should point to some test response data
+        response.json.return_value = str("""[{
+                "id": "12345",
+                "name": "Test doing item",
+                "idList": "67890",
+                "dateLastActivity": "2020-08-01T12:52:06.278Z"
+            }]""")
+
+        return response
+    elif url == f"https://api.trello.com/1/lists/{os.getenv('PENDINGID')}/cards": 
+        response = Mock()
+        
+# sample_trello_lists_response should point to some test response data
+        response.json.return_value = str("""[{
+                "id": "12345",
+                "name": "Test done item",
+                "idList": "67890",
+                "dateLastActivity": "2020-08-01T12:52:06.278Z"
+            }]""")
+
+        return response
+    return None
